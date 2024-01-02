41af6aa1f6bb476df3d9b9aaa0ac64c6af845df0
werdner <werdner@yandex-team.ru>
2024-01-02 15:18:46 +0300

    add solutions for modules 1, 2, 3

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
-  /* ваш код */
+  if (!a || !b) {
+    throw new Error();
+  } else if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError();
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
